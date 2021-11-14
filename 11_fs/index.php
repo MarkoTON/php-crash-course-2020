25a97bb20e1aa5fb2fa43561a8fa2a1cfe30ed67
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-11-14 15:51:10 +0100

    update and comment

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -2,40 +2,46 @@
 // Magic constants
 echo __FILE__ . '<br>';
 echo __DIR__ . '<br>';
-
+echo __LINE__. '<br>';
 // Create directory
-//mkdir('test');
+mkdir('test');
 
 // Rename directory
-//rename('test', 'test2');
+rename('test', 'test2');
 
 // Delete directory
-//rmdir('test2');
+// rmdir('test2');
 
 // Read files and folders inside directory
-//$files = scandir('./');
-//echo '<pre>';
-//var_dump($files);
-//echo '</pre>';
-
+$files = scandir('./');
+echo '<pre>';
+var_dump($files);
+echo '</pre>';
+echo '<br>';
 // file_get_contents, file_put_contents
 $lorem = file_get_contents('lorem.txt');
 echo $lorem;
 echo '<br>';
+file_put_contents('sample.txt', 'Some content');
+echo '<br>';
 file_put_contents('lorem.txt', "First line" . PHP_EOL . $lorem);
-
+echo '<br>';
 // file_get_contents from URL
 $jsonContent = file_get_contents('https://jsonplaceholder.typicode.com/users');
+echo '<br>';
 $users = json_decode($jsonContent);
+echo '<br>';
 var_dump($users);
-
+echo '<br>';
 // Check if file exists or not
 file_exists('lorem.txt'); // true
-
+echo '<br>';
+is_dir('lorem.txt'); // true
+echo '<br>';
 // Get file size
 filesize('lorem.txt');
 
 // Delete file
-unlink('lorem.txt');
+// unlink('lorem.txt');
 
 // https://www.php.net/manual/en/book.filesystem.php
